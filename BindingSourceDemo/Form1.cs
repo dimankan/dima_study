d5fa4c728b4ebcfd39ff3c3b6c971c9342f4b389
dimankan <dimankan94@mail.ru>
2022-03-13 00:59:38 +0300

    no message

---
+++ BindingSourceDemo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BindingSourceDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            usersBindingSource.Add(new Users() { Username = "admin", Password = "admin" });
+            usersBindingSource.Add(new Users() { Username = "admin1", Password = "21wwwa" });
+            usersBindingSource.Add(new Users() { Username = "admin2", Password = "wwaaa" });
+            usersBindingSource.Add(new Users() { Username = "admin3", Password = "331asd" });
+        }
+    }
+}
---
+++ DemoUserControl/ucPeople.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoUserControl
+{
+    public partial class ucPeople : UserControl
+    {
+        public ucPeople()
+        {
+            InitializeComponent();
+        }
+
+        public People SelectedPeople
+        {
+            get
+            {
+                return (People)comboBox1.SelectedItem;
+            }
+        }
+
+        private void ucPeople_Load(object sender, EventArgs e)
+        {
+            List<People> list = new List<People>(); 
+            list.Add(new People() { ID = 1, Name = "Dima"});
+            list.Add(new People() { ID = 2, Name = "Oleg"});
+            list.Add(new People() { ID = 3, Name = "Vanya"});
+            list.Add(new People() { ID = 4, Name = "Misha"});
+            comboBox1.DataSource = list;
+            comboBox1.ValueMember = "ID";
+            comboBox1.DisplayMember = "Name";
+
+        }
+    }
+}
