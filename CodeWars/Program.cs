ef2135d8b1256d08dfed4ccd182bc40e96e4d978
dimankan <dimankan94@mail.ru>
2022-04-24 02:02:33 +0300

    CodeWars

---
+++ CodeWars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 531;
+            decimal result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result = i * result;
+            }
+            string str = result.ToString();
+            char[] chars = str.ToCharArray();
+            Array.Reverse(chars);
+
+            var re = chars.TakeWhile(x => x == '0');
+            Console.WriteLine(re.Count());
+        }
+    }
+}
